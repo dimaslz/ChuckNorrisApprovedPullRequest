1d230ef5d0a90d95f7644728ef69f071e8026f0b
Dimas López Zurita <dimaslopezzurita@gmail.com>
2015-04-28 22:00:32 +0200

    Updated for github

--- script.js
+++ script.js
@@ -3,6 +3,25 @@ $(document).ready(function () {
 	var chucknorrisApproved = chrome.extension.getURL("images/chucknorris.png");
 	var chucknorrisDeclined = chrome.extension.getURL("images/chucknorris_problem.png");
 
+	// animations
+	var chucknorrisApprovedAnimation = function () {
+		$chucknorris.show();
+		$chucknorris
+			.animate({
+				left: "+=0px",
+				width: "500px",
+				height: "500px",
+				display: 'block',
+				top: "+=0px"
+			}, 500 );
+	};
+
+	var chucknorrisStampAnimation = function () {
+		$chucknorris
+			.css({right: 0, bottom: 0, position: "fixed"})
+			.animate({ right: "=0px", bottom: "=0px", width: "200px", height: "200px", display: 'block' }, 500 );
+	};
+
 	// insert images into DOM
 	$('body')
 		.append('<img id="chucknorris" src="'+chucknorrisApproved+'" alt=""/>')
@@ -12,52 +31,112 @@ $(document).ready(function () {
 	var $chucknorris = $('img#chucknorris');
 	var $chucknorris_declined = $('img#chucknorris-problem');
 
-	// get approve button (stash and bitbucket)
-	var approved_btn = $('button.aui-button[class*="approve"]');
+	// get platform
+	var bitbucket = /.*?bitbucket.*/i.test(document.URL);
+	var github = /.*?github.*/i.test(document.URL);
+
+	// get approve button
+	var approved_btn = null;
+	if (github) {
+		approved_btn = $('.commit-form-actions button.btn.btn-primary');
+	} else {
+		approved_btn = $('button.aui-button[class*="approve"]');
+	}
+
 	$(approved_btn).click(function(){
-		$chucknorris
-			.animate({
-				left: "+=0px",
-				width: "500px",
-				height: "500px",
-				display: 'block',
-				top: "+=0px"
-			}, 500 );
+		chucknorrisApprovedAnimation();
 	});
 
 	// get declined button
 	var declined_btn = null;
-	var bitbucket = /.*?bitbucket.*/i.test(document.URL);
 	if (bitbucket) {
 		declined_btn = $('button.aui-button#reject-pullrequest');
+	} else if (github) {
+		//declined_btn = $('button[name="comment_and_close"].btn.js-comment-and-button');
+
+		var checked_declined = false;
+		setInterval(function () {
+			var url = /.*?github.com.*?\/pull\/.*?$/i.test(document.URL);
+			if (url) {
+				declined_btn = $('button[name="comment_and_close"].btn.js-comment-and-button');
+				if (/Close pull request/i.test(declined_btn.text()) && !checked_declined) {
+					checked_declined = true;
+					$(declined_btn).click(function(){
+						checked_declined = false;
+						$chucknorris_declined.show();
+						$chucknorris_declined
+							.animate({ bottom: "+=300px" }, 1500 );
+					});
+				}
+			} else {
+				checked_declined = false;
+				$chucknorris_declined.hide();
+			}
+		},1000);
 	} else {
 		declined_btn = $('button.aui-button.decline-pull-request');
 	}
 
 	$(declined_btn).click(function(){
+		$chucknorris_declined.show();
 		$chucknorris_declined
 			.animate({ bottom: "+=300px" }, 1500 );
 	});
 
-
+	// when load page on merged pull ...
 	var merged = null;
 	if (bitbucket) {
 		merged = $('.pull-request-status .aui-lozenge.aui-lozenge-success').length;
+	} else if (github) {
+		merged = $('.state.state-merged .octicon-git-pull-request').length;
 	} else {
 		merged = $('.pull-request-id-and-state .aui-lozenge.aui-lozenge-success').length;
 	}
 
-	if (merged) {
+	// ... execute the animation
+	if (!!merged) {
 		$chucknorris
 			.css({right: 0, bottom: 0, position: "fixed"})
 			.animate({ right: "=0px", bottom: "=0px", width: "200px", height: "200px", display: 'block' }, 500 );
+	} else { // github work by states and not reload page, so, we check current url to set stamp or hide it
+		if (github) {
+			var checked = false;
+			setInterval(function () {
+				var url = /.*?github.com.*?\/pull\/.*?$/i.test(document.URL);
+				if (url) {
+					merged = $('.state.state-merged .octicon-git-pull-request').length;
+					if (!!merged && !checked) {
+						checked = true;
+						$chucknorris.show();
+						chucknorrisStampAnimation();
+					}
+				} else {
+					checked = false;
+					hideChucknorrisApproved($chucknorris);
+				}
+			},1000);
+		}
 	}
 
+	var hideChucknorrisApproved = function (element) {
+		$(element).hide();
+		$(element).css({
+			width: 10000,
+			height: 10000,
+			display: 'none',
+			right: 'inherit'
+		});
+	};
 	$chucknorris.click(function () {
-		$(this).hide();
+		hideChucknorrisApproved(this);
 	});
 
+
+	var hideChucknorrisProblem = function (element) {
+		$(element).hide();
+		$(element).css({ bottom: -300 });
+	};
 	$chucknorris_declined.click(function () {
-		$(this).hide();
+		hideChucknorrisProblem(this);
 	});
 });
