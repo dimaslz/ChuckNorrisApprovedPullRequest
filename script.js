f9a9ad46306f0c1f11a7d9ffda29b15d720f48ef
Dimas López Zurita <dimaslopezzurita@gmail.com>
2015-05-31 03:28:07 +0200

    Remove Jquery library

--- script.js
+++ script.js
@@ -1,158 +1,153 @@
-$(document).ready(function () {
-	// get images
-	var chucknorrisApproved = chrome.extension.getURL("images/chucknorris.png");
-	var chucknorrisDeclined = chrome.extension.getURL("images/chucknorris_problem.png");
+// get images
+var chucknorrisApprovedStampImage = chrome.extension.getURL("images/chucknorris.png");
+var chucknorrisProblemImage  	  = chrome.extension.getURL("images/chucknorris_problem.png");
 
-	// animations
-	var chucknorrisApprovedAnimation = function () {
-		$chucknorris.show();
-		$chucknorris
-			.animate({
-				left: "+=0px",
-				width: "500px",
-				height: "500px",
-				display: 'block',
-				top: "+=0px"
-			}, 500 );
-	};
+// animations
+var chucknorrisApprovedAnimation = function () {
+	$chucknorrisApprovedStamp.className = $chucknorrisApprovedStamp.className + ' show';
+	setTimeout(function () {
+		$chucknorrisApprovedStamp.className = $chucknorrisApprovedStamp.className + ' approved-animation';
+	}, 1);
+};
 
-	var chucknorrisStampAnimation = function () {
-		$chucknorris
-			.css({right: 0, bottom: 0, position: "fixed", left: 'auto', top: 'auto'})
-			.animate({ right: "=0px", bottom: "=0px", width: "200px", height: "200px", display: 'block', left: '=auto', top: '=auto' }, 500 );
-	};
+var chucknorrisStampAnimation = function () {
+	$chucknorrisApprovedStamp.className = $chucknorrisApprovedStamp.className + 'show';
+	setTimeout(function () {
+		$chucknorrisApprovedStamp.className = $chucknorrisApprovedStamp.className + ' stamp-certified-animation';
+	}, 1);
+};
 
-	// insert images into DOM
-	$('body')
-		.append('<img id="chucknorris" src="'+chucknorrisApproved+'" alt=""/>')
-		.append('<img id="chucknorris-problem" src="'+chucknorrisDeclined+'" alt=""/>');
+// chuck norris stamp element
+var $chucknorrisApprovedStamp = document.createElement('img');
+$chucknorrisApprovedStamp.id  = 'chucknorris';
+$chucknorrisApprovedStamp.src = chucknorrisApprovedStampImage;
 
-	// get current images from DOM
-	var $chucknorris = $('img#chucknorris');
-	var $chucknorris_declined = $('img#chucknorris-problem');
+// chuck norris problem
+var $chucknorrisProblem = document.createElement('img');
+$chucknorrisProblem.id  = 'chucknorris-problem';
+$chucknorrisProblem.src = chucknorrisProblemImage;
 
-	// get platform
-	var bitbucket = /.*?bitbucket.*/i.test(document.URL);
-	var github = /.*?github.*/i.test(document.URL);
+// insert images into DOM
+var body = document.querySelector('body');
+body.appendChild($chucknorrisApprovedStamp);
+body.appendChild($chucknorrisProblem);
 
-	// get approve button
-	var approved_btn = null;
-	if (github) {
-		setInterval(function () {
-			var url = /.*?github.com.*?\/pull\/.*?$/i.test(document.URL);
-			if (url) {
-					approved_btn = $('.commit-form-actions button.btn.btn-primary');
-					approved_btn.click(function () {
-						chucknorrisApprovedAnimation();
-					});
-			}
-		}, 1000);
-	} else {
-		approved_btn = $('button.aui-button[class*="approve"]');
-	}
+// get current images from DOM
+//var $chucknorrisApprovedStamp 		  = chucknorrisElement;
+//var $chucknorrisProblem = chucknorrisProblemElement;
 
-	$(approved_btn).click(function() {
+// get platform
+var bitbucket = /.*?bitbucket.*/i.test(document.URL);
+var github	  = /.*?github.*/i.test(document.URL);
+
+// get approve button
+var approved_btn = null;
+if (github) {
+	setInterval(function () {
+		var url = /.*?github.com.*?\/pull\/.*?$/i.test(document.URL);
+		if (url) {
+			approved_btn = document.querySelector('.state.state-merged');
+			approved_btn.addEventListener('click', function () {
+				chucknorrisApprovedAnimation();
+			});
+		}
+	}, 1000);
+} else {
+	approved_btn = document.querySelector('button.aui-button[class*="approve"]');
+}
+
+if(!!approved_btn) {
+	approved_btn.addEventListener('click', function () {
 		var unapprove = /Unapprove/i.test(approved_btn.text());
 		if (!unapprove) {
 			chucknorrisApprovedAnimation();
 		} else {
 			setTimeout(function () {
-				approved_btn = $('button.aui-button[class*="approve"]');
-				$(approved_btn).on('click', function(){
+				approved_btn = document.querySelector('button.aui-button[class*="approve"]');
+				approved_btn.addEventListener('click', function () {
 					chucknorrisApprovedAnimation();
 				});
 			}, 1000);
 		}
 	});
+}
 
-	// get declined button
-	var declined_btn = null;
-	if (bitbucket) {
-		declined_btn = $('button.aui-button#reject-pullrequest');
-	} else if (github) {
-		//declined_btn = $('button[name="comment_and_close"].btn.js-comment-and-button');
+// get declined button
+var declined_btn = null;
+if (bitbucket) {
+	declined_btn = document.querySelector('button.aui-button#reject-pullrequest');
+} else if (github) {
+	var checked_declined = false;
+	setInterval(function () {
+		var url = /.*?github.com.*?\/pull\/.*?$/i.test(document.URL);
+		if (url) {
+			declined_btn = document.querySelector('button[name="comment_and_close"].btn.js-comment-and-button');
+			if (!!declined_btn && /Close pull request/i.test(declined_btn.text()) && !checked_declined) {
+				checked_declined = true;
+				declined_btn.addEventListener('click', function () {
+					checked_declined = false;
+					$chucknorrisProblem.className = $chucknorrisProblem.className + ' show'; 
+				});
+			}
+		} else {
+			checked_declined = false;
+			$chucknorrisProblem.className = '';
+		}
+	},1000);
+} else {
+	declined_btn = document.querySelector('button.aui-button.decline-pull-request');
+}
 
-		var checked_declined = false;
+if(!!declined_btn) {
+	declined_btn.addEventListener('click', function () {
+		$chucknorrisProblem.className = $chucknorrisProblem.className + ' show';
+	});
+}
+
+// when load page on merged pull ...
+var merged = null;
+if (bitbucket) {
+	merged = document.querySelector('.pull-request-status .aui-lozenge.aui-lozenge-success');
+} else if (github) {
+	merged = document.querySelector('.state.state-merged .octicon-git-pull-request');
+} else {
+	merged = document.querySelector('.pull-request-id-and-state .aui-lozenge.aui-lozenge-success');
+}
+
+// ... execute the animation
+if (!!merged) {
+	chucknorrisStampAnimation();
+} else { // github work by states and not reload page, so, we check current url to set stamp or hide it
+	if (github) {
+		var checked = false;
 		setInterval(function () {
 			var url = /.*?github.com.*?\/pull\/.*?$/i.test(document.URL);
 			if (url) {
-				declined_btn = $('button[name="comment_and_close"].btn.js-comment-and-button');
-				if (/Close pull request/i.test(declined_btn.text()) && !checked_declined) {
-					checked_declined = true;
-					$(declined_btn).click(function(){
-						checked_declined = false;
-						$chucknorris_declined.show();
-						$chucknorris_declined
-							.animate({ bottom: "+=300px" }, 1500 );
-					});
+				merged = document.querySelector('.state.state-merged .octicon-git-pull-request');
+				if (!!merged && !checked) {
+					checked = true;
+					chucknorrisStampAnimation();
 				}
 			} else {
-				checked_declined = false;
-				$chucknorris_declined.hide();
+				checked = false;
+				hideChucknorrisApproved();
 			}
-		},1000);
-	} else {
-		declined_btn = $('button.aui-button.decline-pull-request');
+		}, 1000);
 	}
+}
 
-	$(declined_btn).click(function(){
-		$chucknorris_declined.show();
-		$chucknorris_declined
-			.animate({ bottom: "+=300px" }, 1500 );
-	});
+var hideChucknorrisApproved = function () {
+	$chucknorrisApprovedStamp.className = '';
+};
 
-	// when load page on merged pull ...
-	var merged = null;
-	if (bitbucket) {
-		merged = $('.pull-request-status .aui-lozenge.aui-lozenge-success').length;
-	} else if (github) {
-		merged = $('.state.state-merged .octicon-git-pull-request').length;
-	} else {
-		merged = $('.pull-request-id-and-state .aui-lozenge.aui-lozenge-success').length;
-	}
+$chucknorrisApprovedStamp.addEventListener('click', function () {
+	hideChucknorrisApproved();
+});
 
-	// ... execute the animation
-	if (!!merged) {
-		chucknorrisStampAnimation();
-	} else { // github work by states and not reload page, so, we check current url to set stamp or hide it
-		if (github) {
-			var checked = false;
-			setInterval(function () {
-				var url = /.*?github.com.*?\/pull\/.*?$/i.test(document.URL);
-				if (url) {
-					merged = $('.state.state-merged .octicon-git-pull-request').length;
-					if (!!merged && !checked) {
-						checked = true;
-						$chucknorris.show();
-						chucknorrisStampAnimation();
-					}
-				} else {
-					checked = false;
-					hideChucknorrisApproved($chucknorris);
-				}
-			},1000);
-		}
-	}
+var hideChucknorrisProblem = function () {
+	$chucknorrisProblem.className = '';
+};
 
-	var hideChucknorrisApproved = function (element) {
-		$(element).hide();
-		$(element).css({
-			width: 10000,
-			height: 10000,
-			display: 'none',
-			right: 'inherit'
-		});
-	};
-	$chucknorris.click(function () {
-		hideChucknorrisApproved(this);
-	});
-
-
-	var hideChucknorrisProblem = function (element) {
-		$(element).hide();
-		$(element).css({ bottom: -300 });
-	};
-	$chucknorris_declined.click(function () {
-		hideChucknorrisProblem(this);
-	});
+$chucknorrisProblem.addEventListener('click', function () {
+	hideChucknorrisProblem();
 });
